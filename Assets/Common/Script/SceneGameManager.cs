eb9a3c797043a0828eac6d9fba58c1050bbac3fd
yuggy <yuya.0810@ezweb.ne.jp>
2025-03-02 17:21:33 +0900

    ステージ1_2から開始

--- Assets/Common/Script/SceneGameManager.cs
+++ Assets/Common/Script/SceneGameManager.cs
@@ -34,14 +34,15 @@ public class SceneGameManager : MonoBehaviour
 	{
 		// Stageê∂ê¨
 		Destroy(stageObj);
-		var stageHandle = Addressables.LoadAssetAsync<GameObject>("Stage_1_1");
+		var stageHandle = Addressables.LoadAssetAsync<GameObject>("Stage_1_2");
 		var stage = await stageHandle.Task;
 		stageObj = Instantiate(stage, transform.position, transform.rotation, transform);
+		stageObj.transform.position = new Vector3(stageObj.transform.position.x, -4.0f, stageObj.transform.position.z);
 
 		// Playerê∂ê¨
 		var playerHandle = Addressables.LoadAssetAsync<GameObject>("Player");
 		var player = await playerHandle.Task;
-		var playerPostion = new Vector3(-9, -2, 0);
+		var playerPostion = new Vector3(-9, -5, 0);
 		playerObj = Instantiate(player, playerPostion, transform.rotation, transform);
 	}
 
