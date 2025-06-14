a53389361defb48c06c4deff29baab3ac119a9ee
yuggy <yuya.0810@ezweb.ne.jp>
2025-06-14 19:30:03 +0900

    敵、攻撃されたらプレイヤーを追いかけるよう対応

--- Assets/Object/2_SlashObject/Enemy/Script/EnemyBase.cs
+++ Assets/Object/2_SlashObject/Enemy/Script/EnemyBase.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class EnemyBase : SlashBase
 {
-
+    protected float _chaseVelocity = 0.004f;
+    
+    private void Update()
+    {
+        // 攻撃されたらプレイヤーを追いかける
+        if (IsSlashed)
+        {
+            var playerPos = SceneGameManager.Current.Player.transform.position;
+            var vec = playerPos - transform.position;
+            vec.z = 0;
+            vec.Normalize();
+            transform.position += vec * _chaseVelocity;
+        }
+    }
 }
